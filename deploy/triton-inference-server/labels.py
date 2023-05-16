06472cc2e0b6ff36050bb530588a4e4e441b0a45
aevfly <alyoshine@gmail.com>
2023-05-16 18:24:26 +0300

    main

---
+++ deploy/triton-inference-server/labels.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class COCOLabels(Enum):
+    PERSON = 0
+    CAR = 1
+    MOTORBIKE = 2
+    AEROPLANE = 3
+    BUS = 4
+    TRUCK = 5
+    BOAT = 6
+    BIRD = 7
+    CAT = 8
+    DOG = 9
+    HORSE = 10
+    SHEEP = 11
+    COW = 12
